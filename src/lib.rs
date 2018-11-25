3d7c07e74f0fd6c571dee078dfe432f61cb856a1
Katharina <kookie@spacekookie.de>
2018-11-25 16:33:17 +0100

    Adding better (any) documentation (#7)
    * Adding better documentation to the crate
    * Updating README
    * Updating documentation comments to make Pascal more happy
    * Making the doc tests compile
    * Making the doc comments more pretty
    * Preventing a doc test from exploding

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,61 @@
-//! Easy configuration management
+//! Zero-boilerplate configuration management
 //!
+//! ## Why?
+//!
+//! There are a lot of different requirements when
+//! selecting, loading and writing a config,
+//! depending on the operating system and other
+//! environment factors.
+//!
+//! In many applications this burden is left to you,
+//! the developer of an application, to figure out
+//! where to place the configuration files.
+//!
+//! This is where `confy` comes in.
+//!
+//! ## Idea
+//!
+//! `confy` takes care of figuring out operating system
+//! specific and environment paths before reading and
+//! writing a configuration.
+//!
+//! It gives you easy access to a configuration file
+//! which is mirrored into a Rust `struct` via [serde].
+//! This way you only need to worry about the layout of
+//! your configuration, not where and how to store it.
+//!
+//! [serde]: https://docs.rs/crates/serde
+//!
+//! `confy` uses the [`Default`] trait in Rust to automatically
+//! create a new configuration, if none is available to read
+//! from yet.
+//! This means that you can simply assume your application
+//! to have a configuration, which will be created with
+//! default values of your choosing, without requiring
+//! any special logic to handle creation.
+//!
+//! [`Default`]: https://doc.rust-lang.org/std/default/trait.Default.html
+//!
+//! ```rust
+//! #[derive(Serialize, Deserialize)]
+//! struct MyConfig {
+//!     version: u8,
+//!     api_key: String,
+//! }
+//!
+//! /// `MyConfig` implements `Default`
+//! impl ::std::default::Default for MyConfig {
+//!     fn default() -> Self { Self { version: 0, api_key: "".into() } }
+//! }
+//!
+//! fn main() -> Result<(), ::std::io::Error> {
+//!     let cfg = confy::load("my-app-name")?;
+//! }
+//! ```
+//!
+//! Updating the configuration is then done via the [`store`] function.
+//!
+//! [`store`]: fn.store.html
 //!
 
 extern crate directories;
@@ -15,8 +71,27 @@ use std::fs::{self, File, OpenOptions};
 use std::io::{Error as IoError, ErrorKind::NotFound, Write};
 use std::path::PathBuf;
 
-/// Load a configuration from the standard OS local scope for
-/// the current user.
+/// Load an application configuration from disk
+///
+/// A new configuration file is created with default values if none
+/// exists.
+///
+/// Errors that are returned from this function are I/O related,
+/// for example if the writing of the new configuration fails
+/// or `confy` encounters an operating system or environment
+/// that it does not support.
+///
+/// **Note:** The type of configuration needs to be declared in some way
+/// that is inferrable by the compiler. Also note that your
+/// configuration needs to implement `Default`.
+///
+/// ```rust,no_run
+/// # struct MyConfig {}
+/// # impl ::std::default::Default for MyConf {
+/// #     fn default() -> Self { Self {} }
+/// # }
+/// let cfg: MyConfig = confy::load("my-app")?;
+/// ```
 pub fn load<T: Serialize + DeserializeOwned + Default>(name: &str) -> Result<T, IoError> {
     let project = ProjectDirs::from("rs", name, name);
 
@@ -37,7 +112,27 @@ pub fn load<T: Serialize + DeserializeOwned + Default>(name: &str) -> Result<T, 
     }
 }
 
-/// Store a configuration object
+/// Save changes made to a configuration object
+///
+/// This function will update a configuration,
+/// with the provided values, and create a new one,
+/// if none exists.
+///
+/// You can also use this function to create a new configuration
+/// with different initial values than which are provided
+/// by your `Default` trait implementation, or if your
+/// configuration structure _can't_ implement `Default`.
+///
+/// ```rust,no_run
+/// # struct MyConf {}
+/// let my_cfg = MyConf {};
+/// confy::store(my_cfg)?;
+/// ```
+///
+/// Errors returned are I/O errors related to not being
+/// able to write the configuration file or if `confy`
+/// encounters an operating system or environment it does
+/// not support.
 pub fn store<T: Serialize>(name: &str, cfg: T) -> Result<(), IoError> {
     let project = ProjectDirs::from("rs", name, name);
     fs::create_dir_all(project.config_dir())?;
